deb3041bec38d7d0bd8631f228a19cd363ea0626
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-03-21 23:44:45 +0600

    get value from form

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Home = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
         <div className='min-h-screen bg-indigo-100 w-full flex justify-center items-center'>
             <div className="bg-indigo-200 sm:max-w-md sm:w-full sm:mx-auto sm:rounded-lg sm:overflow-hidden shadow-lg py-5">
@@ -8,15 +14,30 @@ const Home = () => {
                 <h2 className="text-4xl color_heading text-center">QuizMaMa</h2>
                 <h2 className="text-lg color_heading text-center">Test Your Self</h2>
 
-                <form action="#" className='p-4'>
+                <form action="#"
+                    className='p-4'
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <div className="input_group my-3">
                         <label htmlFor="name">Full Name</label>
-                        <input type="text" name="name" id="name" placeholder="Full Name" className="input_design " required />
+                        <input type="text"
+                            name="name"
+                            id="name"
+                            placeholder="Full Name"
+                            className="input_design "
+                            required
+                            {...register("name")}
+                        />
                     </div>
                     <div className="input_group my-3">
                         <label htmlFor="category">Select Category</label>
-                        <select name="category" id="category" className="input_design " required >
-                            <option value="">Test</option>
+                        <select name="category"
+                            id="category"
+                            className="input_design"
+                            required
+                            {...register("category")}
+                        >
+                            <option value="t">Test</option>
                             <option value="">Test</option>
                             <option value="">Test</option>
                             <option value="">Test</option>
@@ -24,8 +45,13 @@ const Home = () => {
                     </div>
                     <div className="input_group my-3">
                         <label htmlFor="level">Choose Level</label>
-                        <select name="level" id="level" className="input_design" required>
-                            <option value="">Test</option>
+                        <select name="level"
+                            id="level"
+                            className="input_design"
+                            required
+                            {...register("level")}
+                        >
+                            <option value="tt">Test</option>
                             <option value="">Test</option>
                             <option value="">Test</option>
                             <option value="">Test</option>
